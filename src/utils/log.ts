6efcc64fb5ad90e5d84cb098d6af895a6f81e53b
Giovanni Ferron <gvnnrulez@gmail.com>
2020-03-27 09:26:38 +1100

    Add long polling

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,6 +1,8 @@
 import { parseJSON, format } from "date-fns";
 import chalk from "chalk";
 import { Timeslot } from "../services/woolies";
+import { getAvailableTimeSlots } from "./timeSlots";
+import config from "config";
 
 export const log = console.log;
 
@@ -9,7 +11,7 @@ const pattern = "PPPP";
 const printFullList = (slot: Timeslot): void => {
   const date = parseJSON(slot.Date);
   log(chalk.bold(format(date, pattern)), "\n");
-  slot.Times.forEach((time) => {
+  slot.Times.forEach(time => {
     if (time.Available) {
       log(time.TimeWindow, "\t", chalk.green(time.TimeSlotStatus));
     } else {
@@ -21,20 +23,14 @@ const printFullList = (slot: Timeslot): void => {
 };
 
 const printAvailable = (data: Timeslot[]): void => {
-  const available = data.reduce((accum, current) => {
-    const avTimes = current.Times.filter((t) => t.Available);
-    if (avTimes.length > 0) {
-      accum.push({ Date: current.Date, Times: avTimes });
-    }
-    return accum;
-  }, [] as Timeslot[]);
+  const available = getAvailableTimeSlots(data);
 
   if (available.length === 0) {
     log(chalk.red.bold("No available times for now, check later!"), "\n");
     return;
   }
 
-  log(chalk.green.bold("YAY! Slots are open!", "\n"));
+  chalk.green.bold("YAY! Slots are open!", config.get("api.baseURL"), "\n");
   available.forEach(printFullList);
 
   log("");
---
+++ src/utils/timeSlots.ts
@@ -0,0 +1,10 @@
+import { Timeslot } from "../services/woolies";
+
+export const getAvailableTimeSlots = (data: Timeslot[]): Timeslot[] =>
+  data.reduce((accum, current) => {
+    const avTimes = current.Times.filter(t => t.Available);
+    if (avTimes.length > 0) {
+      accum.push({ Date: current.Date, Times: avTimes });
+    }
+    return accum;
+  }, [] as Timeslot[]);
